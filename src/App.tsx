b78fa26b9d7dbb320f5094ed290a5e4bbccdf4fa
Alexandre HUMBLOT <humblotalexandre@gmail.com>
2024-10-15 15:11:20 +0200

    remove text

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { CalculatorPage } from "./pages";
 const App = () => {
   return (
     <div className="App">
-      Bouffe mon cul ça fera pas mal de calories
       <CalculatorPage />
     </div>
   );
